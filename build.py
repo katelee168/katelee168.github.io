f3a98e12f290058fccc6fbbb0beba6c9783d2905
Katherine Lee <kate.lee168@gmail.com>
2020-09-28 11:04:02 -0400

    oops, relative path to css

--- build.py
+++ build.py
@@ -1,11 +1,11 @@
 import os
 import subprocess
 
-PARENT_DIR = os.path.abspath(os.getcwd())
+PARENT_DIR = os.getcwd()
 BUILD_ALL = True
 SKIP_FILES = ["README.md"]
 
-def build_in_dir(dir):
+def build_in_dir(dir, relative_dir):
 
 	if BUILD_ALL:
 		files = os.listdir(dir)
@@ -13,7 +13,7 @@ def build_in_dir(dir):
 	# Recurse through directories
 	subdirs = [filename for filename in files if os.path.isdir(filename)]
 	for subdir in subdirs:
-		build_in_dir(os.path.join(dir, subdir))
+		build_in_dir(os.path.join(dir, subdir), '{}/..'.format(relative_dir))
 
 	# Filter for markdown or multi-markdown
 	files = [filename for filename in files if os.path.splitext(filename)[1] in ['.mmd', '.md']]
@@ -27,14 +27,14 @@ def build_in_dir(dir):
 	def get_command(filename, dir):
 		filename = os.path.basename(filename)
 		filebase = '.'.join(filename.split('.')[:-1])
-		CSS_FILENAME = os.path.abspath(os.path.join(dir, "pandoc.css"))
+		CSS_FILENAME = os.path.join(relative_dir, "pandoc.css")
 		print(CSS_FILENAME)
 
 		command = ["pandoc", "-s", 
 					"-c", CSS_FILENAME, 
 					"--filter", "/usr/local/bin/pandoc-crossref", 
 					"--filter", "/usr/local/bin/pandoc-citeproc", 
-					"--include-in-header={}/google-analytics.html".format(PARENT_DIR), 
+					"--include-in-header={}/google-analytics.html".format(relative_dir), 
 					"-o", os.path.join(dir, "{}.html".format(filebase)), 
 					os.path.join(dir, filename)]
 		return command
@@ -44,4 +44,4 @@ def build_in_dir(dir):
 		subprocess.call(get_command(filename, dir))
 
 
-build_in_dir(PARENT_DIR)
+build_in_dir(PARENT_DIR, '.')
