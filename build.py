051af3ffdcf347ab4540c0a0859ccd8db7b2e511
Katherine Lee <kate.lee168@gmail.com>
2025-07-28 15:06:12 -0700

    update

--- build.py
+++ build.py
@@ -30,10 +30,14 @@ def build_in_dir(dir, relative_dir):
 		CSS_FILENAME = os.path.join(relative_dir, "pandoc.css")
 		print(CSS_FILENAME)
 
+		# Determine CSS path and base URL based on directory
+		css_path = "css/newspaper.css" if relative_dir == '.' else "../css/newspaper.css"
+		base_url = "" if relative_dir == '.' else "../"
+		
 		command = ["pandoc", "-s", 
-					"-c", CSS_FILENAME, 
-					"--filter", "/usr/local/bin/pandoc-crossref", 
-					"--filter", "/usr/local/bin/pandoc-citeproc", 
+					"--template={}/template.html".format(PARENT_DIR),
+					"--variable=css-path:{}".format(css_path),
+					"--variable=base-url:{}".format(base_url),
 					"--include-in-header={}/google-analytics.html".format(relative_dir), 
 					"-o", os.path.join(dir, "{}.html".format(filebase)), 
 					os.path.join(dir, filename)]
